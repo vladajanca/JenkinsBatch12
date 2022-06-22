6d0de2bc836785e7602256eca1d26cf856a9d2fa
vladajanca <vladajanca@gmail.com>
2022-06-21 21:43:42 -0400

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         for (int i=0 ;i<5 ;i++){
             System.out.println("hello! am i a cat?");
+            System.out.println("Hello! I am here");
+            System.out.println("hello! I am dog");
         }
     }
 }
